31f1b5fb45da96df38310ac8b3d4461bb15ad871
utkarsh <r.utkarsh.0010@gmail.com>
2024-04-10 04:30:17 +0530

    add client model operations

--- server/src/dao/v1/clients.dao.ts
+++ server/src/dao/v1/clients.dao.ts
@@ -1,13 +1,34 @@
-
-import prisma from "../../utils/database";
-import { IClient } from "../../interfaces/model/client";
+import prisma from '../../utils/database';
+import { IClient } from '../../interfaces/model/client';
 
 class ClientsDao {
+  list(page: number = 1, limit: 10) {
+    return prisma.clients.findMany({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+  }
+
+  findOne(uuid: string) {
+    return prisma.clients.findFirst({ where: { uuid } });
+  }
+
   save(data: IClient) {
     return prisma.clients.create({ data });
   }
 
-  deleteTable() {
+  update(uuid: string, data: Partial<IClient>) {
+    return prisma.clients.update({
+      where: { uuid },
+      data: data,
+    });
+  }
+
+  delete(uuid: string) {
+    return prisma.clients.delete({ where: { uuid } });
+  }
+
+  deleteTable(): Promise<unknown> {
     return prisma.$executeRaw`DROP TABLE clients`;
   }
 }
