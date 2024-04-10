60463922a550f059f6fd1526317fdc11a1b00881
utkarsh <r.utkarsh.0010@gmail.com>
2024-04-10 13:43:05 +0530

    add default client fields

--- client/src/constants/client.ts
+++ client/src/constants/client.ts
@@ -1,9 +1,10 @@
+import dayjs from "dayjs";
 import { IClient } from "../interfaces/Client";
 
 export const DEFAULT_CLIENT_FIELDS: IClient = {
   name: "",
   email: "",
-  registrationDate: "",
+  registrationDate: dayjs().format('YYYY-MM-DD'),
   companyCategory: "",
   companySubCategory: "",
   companyClass: "",
